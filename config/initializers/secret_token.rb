7748d6ffed61604af07b8008c0433355c9c40e9e
lgorse <lgorse@stanford.edu>
2013-10-03 16:54:58 -0700

    initial face commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Face::Application.config.secret_token = 'e0fa5eb58aea3cc11457fc775c9bb94dee99d37245fec6e42ee0bcf526a2f2a7175094ad6ebaf0e20b91c02b9d948708ffa568d6fd0f7f0652891071567a4320'
