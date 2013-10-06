abed19e46cd583bffaa43f0eaaf1e4b3dc470cb7
lgorse <lgorse@stanford.edu>
2013-10-05 21:26:38 -0700

    Implemented Ajax for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,11 @@ class PostsController < ApplicationController
 	before_filter :authenticate
 
 	def create
-		post = Post.create(:user_id => @current_user.id, :text => params[:post][:text])
+		@post = Post.create(:user_id => @current_user.id, :text => params[:post][:text])
 	end
 
 	def destroy
-		Post.find(params[:id]).destroy
+		@post = Post.find(params[:id])
+		@post.destroy
 	end
 end
